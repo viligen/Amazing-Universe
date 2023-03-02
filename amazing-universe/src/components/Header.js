411db94bc89316f28c070d5a7200457fbfe74979
Velina Genova <viligen@gmail.com>
2023-03-02 17:35:14 +0100

    components

--- amazing-universe/src/components/Header.js
+++ amazing-universe/src/components/Header.js
@@ -1,5 +1,5 @@
+import HeroInfo from './HeroInfo';
 import Nav from './Nav';
-import NewPostBtn from './NewPostBtn';
 
 export default function Header() {
     return (
@@ -9,19 +9,7 @@ export default function Header() {
                 <h1>Amazing Universe</h1>
                 <Nav />
             </div>
-            <div className='main-info'>
-                <h2 className='larger'>
-                    Share your favourite<br></br>
-                    space photography
-                </h2>
-
-                <p>
-                    "The universe is a pretty big place. If it's just us, seems
-                    like an awful waste of space."
-                </p>
-                <p>― Carl Sagan, Contact</p>
-                <NewPostBtn />
-            </div>
+            <HeroInfo />
         </header>
     );
 }
---
+++ amazing-universe/src/components/HeroInfo.js
@@ -0,0 +1,19 @@
+import NewPostBtn from "./NewPostBtn";
+
+export default function HeroInfo(){
+    return (
+        <div className='main-info'>
+            <h2 className='larger'>
+                Share your favourite<br></br>
+                space photography, facts and thoughts
+            </h2>
+
+            <p>
+                "The universe is a pretty big place. If it's just us, seems like
+                an awful waste of space."
+            </p>
+            <p>― Carl Sagan, Contact</p>
+            <NewPostBtn />
+        </div>
+    );
+}
