830b826d0c039868f2d8fed90051d7fda1b163de
Velina Genova <viligen@gmail.com>
2023-03-26 13:53:13 +0200

    refactoring, added search to larger collections

---
+++ amazing-universe/src/components/forms/search/SearchForm.js
@@ -0,0 +1,15 @@
+import styles from './SearchForm.module.css';
+
+export default function SearchForm({onSubmit}) {
+    return (
+        <form onSubmit={onSubmit} className={styles.form}>
+            <input type='text' id='search' name='search' defaultValue='' />
+            <button type='reset' className={styles.reset}>
+                x
+            </button>
+            <button type='submit' className={styles.search}>
+                Search
+            </button>
+        </form>
+    );
+}
--- amazing-universe/src/components/posts/list/PostsList.js
+++ amazing-universe/src/components/posts/list/PostsList.js
@@ -7,9 +7,19 @@ import {
 } from '../../../services/postServices';
 
 import styles from './PostsList.module.css';
+import SearchForm from '../../forms/search/SearchForm';
 
 export default function PostsList({ count, owner }) {
     const [posts, setPosts] = useState([]);
+    const [search, setSearch] = useState('');
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const data = Object.fromEntries(new FormData(e.target));
+        setSearch(data.search);
+        // console.log(data.search);
+    };
+
     useEffect(() => {
         if (count) {
             getLimitedPosts(count).then((postsData) => {
@@ -26,13 +36,42 @@ export default function PostsList({ count, owner }) {
         }
     }, [count, owner]);
 
-    return posts.length !== 0 ? (
-        <div className={styles['card-grid']}>
-            {posts.map((p) => (
-                <PostArticle key={p._id} post={p} />
-            ))}
-        </div>
-    ) : (
-        <p className={styles.noPosts}>No Posts to Show</p>
-    );
+    if (search) {
+        const filtered = posts.filter(
+            (p) =>
+                p.title.toLowerCase().includes(search.toLowerCase()) ||
+                p.author.toLowerCase().includes(search.toLowerCase())
+        );
+        return filtered.length !== 0 ? (
+            <>
+                {!count && <SearchForm onSubmit={onSubmit} />}
+                <div className={styles['card-grid']}>
+                    {filtered.map((p) => (
+                        <PostArticle key={p._id} post={p} />
+                    ))}
+                </div>
+            </>
+        ) : (
+            <>
+                {!count && <SearchForm onSubmit={onSubmit} />}
+                <p className={styles.noPosts}>No Posts to Show</p>
+            </>
+        );
+    } else {
+        return posts.length !== 0 ? (
+            <>
+                {!count && <SearchForm onSubmit={onSubmit} />}
+                <div className={styles['card-grid']}>
+                    {posts.map((p) => (
+                        <PostArticle key={p._id} post={p} />
+                    ))}
+                </div>
+            </>
+        ) : (
+            <>
+                {!count && <SearchForm onSubmit={onSubmit} />}
+                <p className={styles.noPosts}>No Posts to Show</p>
+            </>
+        );
+    }
 }
